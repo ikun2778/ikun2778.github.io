7dc506d95cab03469fa69c756791173004b074c8
ikun2778 <1589209056@qq.com>
2024-12-21 17:13:21 +0800

    Site updated: 2024-12-21 17:13:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/19/hello-world/","2024/12/20/交换排序/","2024/12/19/专升本转段考试相关通知/","2024/12/19/加密与解密/","2024/12/20/冒泡排序/","2024/12/20/杨辉三角/"];function toRandomPost(){
+var posts=["2024/12/20/bubbleSort/","2024/12/19/encode and decode/","2024/12/19/hello-world/","2024/12/20/pascal/","2024/12/20/swapSort/","2024/12/19/zsb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
