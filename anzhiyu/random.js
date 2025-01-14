14ea8f5a2cb5660494e8c5d3d1b6652ada22c58f
ikun2778 <1589209056@qq.com>
2025-01-14 11:56:04 +0800

    Site updated: 2025-01-14 11:56:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/19/hello-world/","2025/01/01/客观题/","2025/01/05/数据结构/","2024/12/30/编程题/"];function toRandomPost(){
+var posts=["2024/12/19/hello-world/","2025/01/05/数据结构/","2024/12/30/编程题/","2025/01/01/客观题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
